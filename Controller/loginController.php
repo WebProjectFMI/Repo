9cea2fcb873c1629a6fd6d7f5666605610b87d13
Teodor <teodor.yolovski@gmail.com>
2020-02-08 20:10:09 +0200

    Refactoring

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -8,6 +8,6 @@ function authenticate($username, $password) {
             return extractUserId($username);
         }
     }   
-    return FALSE;
+    return 0;
 }
 ?>
--- Model/adminModel.php
+++ Model/adminModel.php
@@ -32,4 +32,5 @@ function extractUsername($uID) {
     return $result['username'];
 }
 
+
 ?>
