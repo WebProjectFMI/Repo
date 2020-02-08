2c40559f128ae00a7e05c4910fa8f082818bd473
tkgrozdanov <teodor.grozdanov@gmail.com>
2020-02-08 17:54:53 +0200

    Add mainPageController.php file

---
+++ Controller/mainPageController.php
@@ -0,0 +1,27 @@
+<?php
+require '../Model/mainPageModel.php';
+
+function getUserCorrs($uID) {
+    return extractUserCorrs($uID);
+}
+
+function getCorrEmails($corrID, $uID) {
+    if (belongsUserCorr($uID, $corrID)) {
+        return extractCorrEmails($corrID);
+    }
+    return [];
+}
+
+function sendEmail($corrID, $fromUID, $subject, $content) {
+    if ((is_int($corrID) || ctype_digit($corrID)) && belongsUserCorr($fromUID, $corrID)) {
+        insertEmail($corrID, $fromUID, $subject, $content);
+    }
+}
+function getUnread($uID) {
+    return extractUnread($uID);
+}
+
+function createCorr($title, $users) {
+    insertCorr($users, $title);
+}
+?>
