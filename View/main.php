071a3573158bd0be7ce3307b1821d455cc9f3907
ToDoRV <tr.velikov@gmail.com>
2020-02-09 01:00:30 +0200

    Add main.php file

---
+++ View/main.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            box-sizing: border-box;
+            font-family: sans-serif;
+        }
+
+        body {
+        }
+
+        .header {
+            background-color: #7acf05;
+            padding: 2%;
+            text-align: right;
+            font-size: 16px;
+            color: white;
+            height: 9vh;
+        }
+
+        .navigation {
+            height: 40px;
+            padding-top: 10px;
+            padding-left: 10px;
+            overflow: hidden;
+        }
+
+        .correspondences {
+            float: left;
+            padding: 20px;
+        }
+
+        .navigation, .correspondences {
+            width: 30%;
+            background: #ccc;
+        }
+
+        .correspondences ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        .correspondences ul li {
+            list-style-type: none;
+            text-align: center;
+            cursor: pointer;
+            padding: 9px;
+        }
+
+        .emailContent {
+            float: right;
+            width: 70%;
+            padding: 5%;
+            background-color: #f4f6ef;
+            text-align: justify;
+        }
+
+        .correspondences, .emailContent {
+            height: 85vh;
+            overflow: auto;
+        }
+
+        .emailContent .receivedMessage {
+            text-align: left;
+        }
+
+        .emailContent .sentMessage {
+            text-align: right;
+        }
+
+    </style>
+</head>
+<body>
+<dev>
+    <header class="header">
+        <div>username</div>
+    </header>
+
+    <nav class="navigation">
+        <button type="button" onclick="alert('Нова кореспонденция')">Нова кореспонденция</button>
+    </nav>
+
+    <section>
+        <nav class="correspondences">
+            <ul>
+                <li aria-selected="true">
+                    <div onclick="alert('Имейл')">Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+                <li>
+                    <div>Кореспонденция1</div>
+                </li>
+            </ul>
+        </nav>
+        <main class="emailContent">
+            <div class="receivedMessage">
+                <p>Здравей, как си?</p>
+            </div>
+            <div class="sentMessage">
+                <p>Здравей, добре.</p>
+            </div>
+        </main>
+    </section>
+
+</dev>
+</body>
+</html>
