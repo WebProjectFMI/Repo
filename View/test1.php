8cc626451846fe0607abe72cec6a11a7358f40ab
Teodor <teodor.yolovski@gmail.com>
2020-02-09 09:20:12 +0200

    Add back button from admin page to main page

---
+++ View/test1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Example</title>
+</head>
+<body>
+
+<?php
+require("../model/dbConfig.php");
+
+echo "<h2>Hi, I'm a PHP script!</h2>";
+
+    $sql = "SELECT username FROM users;";   
+    echo "<br/>";
+    $query = $connection->query($sql) or die("failed!");
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    $username =  $row['username'];
+    echo $username;
+    echo "<br/>"; 
+}
+?>
+</body>
+</html>
